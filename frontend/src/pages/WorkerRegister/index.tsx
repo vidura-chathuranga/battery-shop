36c985c8c975716ff8fefd249351804e77439a6f
Sahan Bandara <92173358+sahanbandara23623@users.noreply.github.com>
2023-07-13 16:09:23 +0530

    Registration Validation and Admin Dashboard
    Registration Validation and Admin Dashboard

--- frontend/src/pages/WorkerRegister/index.tsx
+++ frontend/src/pages/WorkerRegister/index.tsx
@@ -7,8 +7,52 @@ import {
   Button,
   Select,
 } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { useState } from "react";
 
-const register = () => {
+const Register = () => {
+  const registerForm = useForm({
+    validateInputOnChange: true,
+
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+      phone: "",
+      nic: "",
+      address: "",
+      gender: "",
+    },
+
+    //Validate Data in real time
+
+    validate: {
+      name: (value) =>
+        value.length < 2 ? "Name must have at least 2 letters" : null,
+      email: (value) =>
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+          value
+        )
+          ? null
+          : "Invalid email",
+
+      phone: (value) =>
+        /^\d{10}$/.test(value)
+          ? null
+          : "Phone number must be 10 digits long number",
+
+        nic:(value)=>{
+          if(!value){
+            return 'This field is Required'
+          }
+          if (!/^\S+@\S+$/.test(value) && !/^([0-9]{9}[v|V]|[0-9]{12})$/.test(value)) {
+            return 'Invalid email or NIC';
+          }
+          return null;
+        }
+    },
+  });
+
   return (
     <Container size={420} my={40}>
       <Title
@@ -22,26 +66,54 @@ const register = () => {
       </Title>
 
       <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-        <TextInput label="Email" placeholder="you@mantine.dev" required />
+        <form onSubmit={registerForm.onSubmit((values:any)=>{})}>
+
+        <TextInput label="Name" placeholder="Enter name" name="name"  required  {...registerForm.getInputProps("name")}/>
+        <TextInput
+          label="Email"
+          placeholder="you@mantine.dev"
+          name="email"
+          required
+          {...registerForm.getInputProps("email")}
+        />
         <PasswordInput
+          name="password"
           label="Password"
           placeholder="Your password"
           required
           mt="md"
+          {...registerForm.getInputProps("password")}
         />
 
-        <TextInput label="Phone" placeholder="Enter phone" required mt="md" />
-
-        <TextInput label="NIC" placeholder="Enter phone" required mt="md" />
+        <TextInput
+          label="Phone"
+          placeholder="Enter phone"
+          name="phone"
+          required
+          mt="md"
+          {...registerForm.getInputProps("phone")}
+        />
 
         <TextInput
+          label="NIC"
+          placeholder="Enter NIC"
+          name="nic"
+          required
+          mt="md"
+          {...registerForm.getInputProps("nic")}
+        />
+
+        <TextInput
+          name="address"
           label="Address"
           placeholder="Enter address"
           required
           mt="md"
+          {...registerForm.getInputProps("address")}
         />
 
         <Select
+          name="gender"
           label="Gender"
           placeholder="Select gender"
           required
@@ -49,14 +121,18 @@ const register = () => {
             { value: "male", label: "Male" },
             { value: "female", label: "Female" },
           ]}
+          {...registerForm.getInputProps("gender")}
         />
 
-        <Button fullWidth mt="xl">
+
+        <Button fullWidth mt="xl" type="submit">
           Sign Up
         </Button>
+
+        </form>
       </Paper>
     </Container>
   );
 };
 
-export default register;
+export default Register;
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -3,6 +3,7 @@ import LandingPage from '../pages/LandingPage';
 import AdminLoginPage from '../pages/Login/adminLogin';
 import WorkerLoginPage from '../pages/Login/workerLogin';
 import Workerregister from '../pages/WorkerRegister';
+import ADashboard from '../pages/AdminDashboard';
 
 
 const AllRoutes = () =>{
@@ -13,6 +14,8 @@ const AllRoutes = () =>{
                 <Route path='/login/admin' element={<AdminLoginPage/>}/>
                 <Route path='/login/worker' element={<WorkerLoginPage/>}/>
                 <Route path='/login/register' element={<Workerregister/>}/>
+                <Route path='/login/adminDashboard' element={<ADashboard/>}/>
+
             </Routes>
         </Router>        
     );

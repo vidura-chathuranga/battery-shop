d42c685ff39a917b6dc98af1678781e3b674b9ee
Vinnath Pathirana <125301413+VinnathPathirana@users.noreply.github.com>
2023-07-19 05:46:18 +0530

    add a background

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,10 +1,12 @@
 import {Group } from "@mantine/core";
 import WorkerLogin from "../../components/workerLogin";
 import AdminLogin from "../../components/adminLogin";
+import backgroundimg from "../../assets/backgroundimg.jpg"
 
 const LoginPage = () => {
   return (
     <div >
+       <img src={backgroundimg} alt="Background" style={{ width: '100%', height: '100vh', objectFit: 'cover' }} />
       <Group position="center" m={30} spacing={"lg"}>
         <AdminLogin />
         <WorkerLogin />

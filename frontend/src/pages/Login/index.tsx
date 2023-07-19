9f87eee5d991cb1d3516f8c9bab7b9d404e47c94
Vinnath Pathirana <125301413+VinnathPathirana@users.noreply.github.com>
2023-07-19 05:55:23 +0530

    landing page edit

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -5,8 +5,8 @@ import backgroundimg from "../../assets/backgroundimg.jpg"
 
 const LoginPage = () => {
   return (
-    <div style={{ height: "100vh" }}>
-      <Group position="center" mt={250} spacing={100}>
+    <div style={{ height: "100vh" , backgroundImage:`url(${backgroundimg})`, backgroundSize: 'cover', backgroundPosition: 'center', display: 'flex', justifyContent: 'center', alignItems: 'center'  }}>
+      <Group position="center"  spacing={100}>
           <AdminLogin />
           <WorkerLogin />
       </Group>

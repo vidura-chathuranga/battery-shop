b2f737ead29e01454a0b7c05cba1198f8f8c7c70
Sahan Bandara <92173358+sahanbandara23623@users.noreply.github.com>
2023-07-22 13:33:45 +0530

    small updates
    small updates

--- frontend/src/components/profitChart/chart.tsx
+++ frontend/src/components/profitChart/chart.tsx
@@ -1,16 +1,14 @@
 import {Line} from 'react-chartjs-2';
 
 import {
-    Chart as ChartJS,
-    LineElement,
-    CategoryScale,
-    LinearScale,
-    PointElement
+  Chart as ChartJS,
+  LineElement,
+  LinearScale,
+  PointElement
 } from 'chart.js';
 
 ChartJS.register(
     LineElement,
-    CategoryScale,
     LinearScale,
     PointElement,
 )

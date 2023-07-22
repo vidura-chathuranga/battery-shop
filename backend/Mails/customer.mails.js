9e4ee4f9b7330da23e9ca8672d09bb972390f5cb
Vidura Chathuranga Thilakasiri <vidurachathuranga2020@gmail.com>
2023-07-22 18:39:32 +0530

    created email template

--- backend/Mails/customer.mails.js
+++ backend/Mails/customer.mails.js
@@ -1,68 +1,98 @@
 import mailgen from "mailgen";
 import { mailConfigs } from "../configs/nodeMailer.config.js";
-import 'dotenv/config';
-import { table } from "console";
+import "dotenv/config";
 
 export const sendInvoiceMail = async (invoice) => {
+  // price formatter
+  let rupee = new Intl.NumberFormat("ta-LK", {
+    style: "currency",
+    currency: "LKR",
+  });
 
-    //import mail configs
-    let mailTransporter = mailConfigs();
+  //import mail configs
+  let mailTransporter = mailConfigs();
 
-    let MailGenerator = new mailgen({
-        theme: "cerberus",
-        product: {
-            name: "Battery shop name",
-            link: 'http://localhost:3000/',
-            logo: 'https://drive.google.com/file/d/1iediwMBZTLS9fS4vkezgULwzUcTAA0fe/view?usp=drive_link'
-        }
+  let MailGenerator = new mailgen({
+    theme: "cerberus",
+    product: {
+      name: "Battery shop name",
+      link: "http://localhost:3000/",
+      logo: 'https://drive.google.com/file/d/14rDrjOsL3Co8bWG2Zu6qNlubbD7OuZSW/view?usp=sharing',
+    },
+  });
 
-
-    });
-
-
-
-    var email = {
-        
-        body: {
-            name: `${invoice.cusName}`,
-            intro: `Here your invoice`,
-            table: {
-
-                data: [{
-
-                    brand: `${invoice.items.brand}`,
-                    warranty: `${invoice.items.warranty}`,
-                    price_of_unit: `${invoice.items.price}`,
-                    Quantity: `${invoice.items.quantity}`,
-                    total_price: `${invoice.items.totalPrice}`,
-                    discount: `${invoice.discount}`,
-                    amount_to_be_pay: `${invoice.totalSoldPrice}`,
-
-                },
-                ]
-            },
-            outro: 'Need help, or have questions? Just reply to this email, we\'d love to help.'
-
-        }
+  //generating table data
+  const tableData = invoice.items.map((item) => {
+    return {
+      Brand: item.brand,
+      Warranty: item.warranty,
+      "Unit price": rupee.format(item.price).toString(),
+      Quantity: item.quantity.toString(),
+      "Total price": rupee.format(item.totalPrice).toString(),
     };
+  });
 
-    //convert mailgen body into HTML
-    let mail = MailGenerator.generate(email);
+  var email = {
+    body: {
+      name: `${invoice.cusName}`,
+      intro:
+        "We sincerely appreciate your business. Attached is your invoice for purchasing batteries from Sensus hub.Thank you for choosing us, and we look forward to serving you again in the future.",
+      table: {
+        data: [
+          ...tableData,
+          {
+            Brand: "",
+            Warranty: "",
+            "Unit price": "Discount",
+            Quantity: "",
+            "Total price": `<b>- ${rupee
+              .format(invoice.discount)
+              .toString()}</b>`,
+          },
+          {
+            Brand: "",
+            Warranty: "",
+            "Unit price": "Total price",
+            Quantity: "",
+            "Total price": `<b>${rupee
+              .format(invoice.totalSoldPrice)
+              .toString()}</b>`,
+          },
+        ],
+        columns:{
+          customAlignment:{
+            "Unit price" : 'right',
+            'Quantity' : 'center',
+             "Total price" : 'right'
+          }
+          
+        }
+      },
+      outro:
+        "Thank you once again for choosing Sensus Hub.We look forward to serving you again in the near future. Have a wonderful day!",
+    },
+  };
 
+  //convert mailgen body into HTML
+  let emailBody = MailGenerator.generate(email);
+  let emailText = MailGenerator.generatePlaintext(email);
 
-    //sending credentials
-    let details = {
-        from: process.env.CLIENT_EMAIL,
-        to: `${invoice.cusEmail}`,
-        subject: `Invoice`,
-        html: mail
-    }
+  //sending credentials
+  let details = {
+    from: process.env.CLIENT_EMAIL,
+    to: `${invoice.cusEmail}`,
+    subject: `Battery Invoice`,
+    html: emailBody,
+    text: emailText,
+  };
 
-    //send mail through nodemailer
-    await mailTransporter.sendMail(details).then((data) => {
-        return data;
-    }).catch((error) => {
-        return error;
+  //send mail through nodemailer
+  await mailTransporter
+    .sendMail(details)
+    .then((data) => {
+      return data;
     })
+    .catch((error) => {
+      return error;
+    });
 };
-

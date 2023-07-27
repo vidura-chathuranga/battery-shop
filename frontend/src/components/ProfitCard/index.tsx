2603f7853c4231a89367323aff3eab6993aee3ef
vidura-chahuranga <vidurachathuranga2020@gmail.com>
2023-07-27 17:04:32 +0530

    created weekly profit chart

--- frontend/src/components/ProfitCard/index.tsx
+++ frontend/src/components/ProfitCard/index.tsx
@@ -20,11 +20,11 @@ import { DateInput } from "@mantine/dates";
 import { useForm } from "@mantine/form";
 import { showNotification } from "@mantine/notifications";
 import { IconX } from "@tabler/icons-react";
-import profitImage from "../../assets/profit.png"
-import BarryImage from "../../assets/BattaryImage.png"
+import profitImage from "../../assets/profit.png";
+import BarryImage from "../../assets/BattaryImage.png";
 import Chart from "../profitChart/chart";
-import { MonthPickerInput } from '@mantine/dates';
-
+import { MonthPickerInput } from "@mantine/dates";
+import WeekProfitChart from "../Charts/weekProfitChart";
 
 const useStyles = createStyles((theme) => ({
   label: {
@@ -35,8 +35,8 @@ const useStyles = createStyles((theme) => ({
   },
 
   cardsContainer: {
-    display: 'flex',
-    justifyContent: 'center',
+    display: "flex",
+    justifyContent: "center",
     gap: theme.spacing.lg,
     marginTop: theme.spacing.xl,
   },
@@ -44,7 +44,7 @@ const useStyles = createStyles((theme) => ({
   customCard: {
     padding: theme.spacing.xl, // Adjust the padding as needed to increase the card size
     borderRadius: theme.radius.md,
-    width: '500px', // Set a fixed width for the cards or adjust as per your requirement
+    width: "500px", // Set a fixed width for the cards or adjust as per your requirement
   },
 
   dateInputContainer: {
@@ -58,18 +58,16 @@ const useStyles = createStyles((theme) => ({
     width: "100%",
   },
 
-  monthInput:{
+  monthInput: {
     maxWidth: 200,
     width: "100%",
     justifyContent: "center",
-
   },
 
   card: {
     backgroundColor:
       theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
   },
-
 
   lead: {
     fontFamily: `Greycliff CF, ${theme.fontFamily}`,
@@ -98,7 +96,6 @@ const useStyles = createStyles((theme) => ({
   },
 }));
 
-
 // Helper function to check if a date is within the last 7 days from the selected date
 function isWithinLast7Days(selectedDate: any, targetDate: any) {
   const sevenDaysAgo = new Date(selectedDate);
@@ -114,8 +111,13 @@ function isWithinMonth(selectedDate: any, targetDate: any) {
   return targetDate >= startOfMonth && targetDate <= endOfMonth;
 }
 
-
-
+// generate random colors for charts not too close to white
+const getRandomRGBColor = () => {
+  const r = Math.floor(Math.random() * 206) + 50; // Random value between 50 and 255 for red
+  const g = Math.floor(Math.random() * 206) + 50; // Random value between 50 and 255 for green
+  const b = Math.floor(Math.random() * 206) + 50; // Random value between 50 and 255 for blue
+  return `rgb(${r}, ${g}, ${b})`;
+};
 
 // title, completed, total, stats
 export function StatsProfitCard() {
@@ -127,6 +129,23 @@ export function StatsProfitCard() {
   const [monthItemCount, setMonthItemCount] = useState(0);
   const [monthProfit, setMonthProfit] = useState(0);
   const [value, setValue] = useState<Date | null>(null);
+  const [weekProfitData, setWeekProfitData] = useState({
+    labels: [""],
+    datasets: [
+      {
+        label: "Weekly Profit(Rs)",
+        data: [0],
+        borderColor: getRandomRGBColor(),
+        backgroundColor: getRandomRGBColor(),
+      },
+    ],
+  });
+
+  // format number to SL rupee
+  let rupee = new Intl.NumberFormat("ta-LK", {
+    style: "currency",
+    currency: "LKR",
+  });
 
   // use react query and fetch data
   const { data, isLoading, isError } = useQuery(["invoiceData"], () => {
@@ -148,6 +167,79 @@ export function StatsProfitCard() {
     });
   }
 
+  const generateChartData = (selectedDate: any) => {
+    //generating the labels
+    const currrentDate = new Date(selectedDate); //convert selected date into date object
+    let lastDay = new Date();
+
+    lastDay.setDate(currrentDate.getDate() - 7); // set lastday date as 7 day ago date
+
+    let chartData: any = []; // defining the chart data array
+    const weekDays = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    // looping 8 times to generate the structure of the chart data and generate the chart labels
+    for (let i = 0; i < 8; i++) {
+      chartData.push({
+        label: `${weekDays[lastDay.getDay()]}-${lastDay.getDate()}`,
+        profit: 0,
+      });
+      lastDay.setDate(lastDay.getDate() + 1);
+    }
+
+    // again reset the lastDay value
+    lastDay.setDate(currrentDate.getDate() - 7);
+
+    // looping through the invoice data and calculating total profit for each day
+    data?.map((invoice: any) => {
+      const issuedDate = new Date(invoice.issuedDate); //converting the invoice issued date into date object
+
+      if (issuedDate >= lastDay && issuedDate <= currrentDate) {
+        // comparing the issued date inbetween the selected week
+
+        // if YES, then calculate the total profit of the particular date
+        const updateChartData = chartData.map((dayData: any) => {
+          const labelDate = dayData.label.split("-")[1];
+          if (labelDate === issuedDate.getDate().toString()) {
+            return {
+              ...dayData,
+              profit:
+                dayData.profit +
+                (parseFloat(invoice.totalSoldPrice) -
+                  parseFloat(invoice.totalActualPrice)),
+            };
+          } else {
+            return dayData;
+          }
+        });
+        chartData = updateChartData; // then reassign the updated data into the chartData variable
+      }
+    });
+
+    const newChartData = {
+      ...weekProfitData,
+      labels: chartData.map((day: any) => day.label),
+      datasets: [
+        {
+          ...weekProfitData.datasets[0],
+          data: chartData.map((day: any) => day.profit),
+          borderColor: getRandomRGBColor(),
+          backgroundColor: getRandomRGBColor(),
+        },
+      ],
+    };
+
+    // console.log(newChartData);
+    setWeekProfitData(newChartData);
+  };
+
   // Calculate last 7 days profit and item count
   const calculateLast7DaysProfitAndItemCount = (selectedDate: any) => {
     setWeekProfit(0);
@@ -157,13 +249,18 @@ export function StatsProfitCard() {
       const issuedDate = new Date(invoice.issuedDate);
 
       if (isWithinLast7Days(selectedDate, issuedDate)) {
-        setWeekProfit((prev) => prev + invoice.totalSoldPrice - invoice.totalActualPrice);
+        setWeekProfit(
+          (prev) => prev + invoice.totalSoldPrice - invoice.totalActualPrice
+        );
 
         invoice.items.forEach((items: any) => {
           setWeekItemCount((prev) => prev + items.quantity);
         });
       }
     });
+
+    // generateChartData
+    generateChartData(selectedDate);
   };
 
   // Calculate Monthly Profit
@@ -175,7 +272,9 @@ export function StatsProfitCard() {
       const issuedDate = new Date(invoice.issuedDate);
 
       if (isWithinMonth(date, issuedDate)) {
-        setMonthProfit((prev) => prev + invoice.totalSoldPrice - invoice.totalActualPrice);
+        setMonthProfit(
+          (prev) => prev + invoice.totalSoldPrice - invoice.totalActualPrice
+        );
 
         invoice.items.forEach((items: any) => {
           setMonthItemCount((prev) => prev + items.quantity);
@@ -183,8 +282,6 @@ export function StatsProfitCard() {
       }
     });
   };
-
-
 
   //calculate profit function
   const calculateProfit = (date: Date) => {
@@ -209,7 +306,6 @@ export function StatsProfitCard() {
     });
   };
 
-
   return (
     <>
       <div className={classes.dateInputContainer}>
@@ -221,16 +317,21 @@ export function StatsProfitCard() {
           withAsterisk
           onChange={calculateProfit}
           style={{
-            width: '500px',
-            borderRadius: '25px',
+            width: "500px",
+            borderRadius: "25px",
           }}
         />
       </div>
 
       <div className={classes.cardsContainer}>
-        <Group position="apart">
+        <Group position="center" spacing={"lg"}>
           {/* Apply the customCard style to the first Card */}
-          <Card className={classes.customCard} shadow="sm" radius="md" withBorder>
+          <Card
+            className={classes.customCard}
+            shadow="sm"
+            radius="md"
+            withBorder
+          >
             <center>
               {/* Add an Image inside the center tags */}
               <Image
@@ -249,7 +350,12 @@ export function StatsProfitCard() {
           </Card>
 
           {/* Apply the customCard style to the second Card */}
-          <Card className={classes.customCard} shadow="sm" radius="md" withBorder>
+          <Card
+            className={classes.customCard}
+            shadow="sm"
+            radius="md"
+            withBorder
+          >
             <center>
               {/* Add an Image inside the center tags */}
               <Image
@@ -269,7 +375,16 @@ export function StatsProfitCard() {
 
           {/* Apply the customCard style to the Third Card */}
           {/* Calculate weekly profit and item count */}
-          <Card className={classes.customCard} shadow="sm" radius="md" withBorder style={{ height: '380px' }}>
+          <Card
+            className={classes.customCard}
+            shadow="sm"
+            radius="md"
+            withBorder
+            style={{ height: "430px" }}
+          >
+            <Text weight={500} size={30}>
+              <center>WEEKLY PROFIT & ITEM COUNT</center>
+            </Text>
             <div className={classes.dateInputContainer}>
               <DateInput
                 className={classes.dateInput}
@@ -277,40 +392,50 @@ export function StatsProfitCard() {
                 label="Choose Date to view profit"
                 valueFormat="YYYY MMM DD"
                 withAsterisk
+                size="xs"
                 onChange={calculateLast7DaysProfitAndItemCount} // Update the onChange function to the new one
                 style={{
-                  width: '500px',
-                  borderRadius: '25px',
+                  width: "500px",
+                  borderRadius: "25px",
                 }}
               />
             </div>
 
-            <Text weight={500} size={30}>
-              <center>WEEKLY PROFIT & ITEM COUNT</center>
-            </Text>
-
-            {weekItemCount === 0 && weekProfit === 0 ?(
-
-            <Text weight={600}  size={20} color="red">
-             <center>No data</center>
-             </Text>
-
-            ): (
-
-            <Text weight={600} size={20} color="blue">
-              <center>Item Count - {weekItemCount}</center>
-              <center> Rs.{weekProfit}</center>
-            </Text>
+            {weekItemCount === 0 && weekProfit === 0 ? (
+              <Text weight={600} size={20} color="red">
+                <center>No data</center>
+              </Text>
+            ) : (
+              <Text weight={600} size={20} color="blue">
+                <center>Item Count - {weekItemCount}</center>
+                <center> Rs.{weekProfit}</center>
+              </Text>
             )}
           </Card>
 
-
+          <Card
+            className={classes.customCard}
+            shadow="sm"
+            radius="md"
+            withBorder
+            style={{ height: "430px" }}
+          >
+            <Text weight={500} size={30} mb={40}>
+              <center>WEEKLY PROFIT</center>
+            </Text>
+            <WeekProfitChart profitData={weekProfitData} />
+          </Card>
           {/* Apply the customCard style to the Third Card */}
           {/* Calculate weekly profit and item count */}
-          <Card className={classes.customCard} shadow="sm" radius="md" withBorder style={{ height: '380px' }}>
+          <Card
+            className={classes.customCard}
+            shadow="sm"
+            radius="md"
+            withBorder
+            style={{ height: "430px" }}
+          >
             <div className={classes.dateInputContainer}>
               <MonthPickerInput
-                
                 className={classes.dateInput}
                 placeholder="Choose a Month"
                 label="Choose a Month to view profit"
@@ -318,35 +443,41 @@ export function StatsProfitCard() {
                 withAsterisk
                 onChange={calculateMonthlyProfitAndItemCount} // Update the onChange function to the new one
                 style={{
-                  width: '500px',
-                  borderRadius: '25px',
+                  width: "500px",
+                  borderRadius: "25px",
                 }}
               />
-
             </div>
 
             <Text weight={500} size={30}>
               <center>MONTHLY PROFIT & ITEM COUNT</center>
             </Text>
-            
-            {monthItemCount === 0 && monthProfit === 0? (
-                 <Text weight={600}  size={20} color="red">
-                  <center>No data</center>
-                 </Text>
+
+            {monthItemCount === 0 && monthProfit === 0 ? (
+              <Text weight={600} size={20} color="red">
+                <center>No data</center>
+              </Text>
             ) : (
-            <Text weight={600} size={20} color="blue">
-              <center>Item Count - {monthItemCount}</center>
-              <center> Rs.{monthProfit}</center>
-            </Text>
+              <Text weight={600} size={20} color="blue">
+                <center>Item Count - {monthItemCount}</center>
+                <center> Rs.{monthProfit}</center>
+              </Text>
             )}
-
           </Card>
-            
-
+          <Card
+            className={classes.customCard}
+            shadow="sm"
+            radius="md"
+            withBorder
+            style={{ height: "430px" }}
+          >
+            <Text weight={500} size={30} mb={40}>
+              <center>WEEKLY PROFIT</center>
+            </Text>
+            <WeekProfitChart profitData={weekProfitData} />
+          </Card>
         </Group>
-
       </div>
-
     </>
   );
 }
